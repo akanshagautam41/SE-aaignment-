a523b69b3e4bf69cf81f054e3e42380aab495c8b
akanshagautam41 <126063651+akanshagautam41@users.noreply.github.com>
2023-03-09 22:44:30 +0530

    Delete Dateaddressname.c.c

--- Dateaddressname.c.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{ 
-printf("\n enter your name: akansha");
-printf("\n enter your address: vadodara");
-printf("\n enter your birthdate: 28april");
-printf("\n enter your age:28");
-}
